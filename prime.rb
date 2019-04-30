873b1f3d21e3361014852d4778b2350156f76f3d
Erika Davis <davis2erika@gmail.com>
2019-04-30 22:26:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,3 +2,14 @@ def prime?(number)
   (2..number -1).each {|n| return false if number <= 1 || number % n == 0}
   return true
 end
+
+def prime?(number)
+return false if number <= 1
+  (2..number-1).each do |n|
+  if number % n == 0
+  return false
+   end
+end  
+true
+end
+  
